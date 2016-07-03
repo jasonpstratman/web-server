a76abedaa7a525693327733ce2a30c9870ec5c3d
Jason Stratman <jpstratman@gmail.com>
2016-07-03 10:43:35 -0500

    Calculator now only allows numerical input and decimal points.

--- public/javascripts/calculator.js
+++ public/javascripts/calculator.js
@@ -51,6 +51,16 @@ var topnum = 0;
 var bottomnum = 0;
 var fresh = 0;
 
+function isNumberKey(evt)
+       {
+          var charCode = (evt.which) ? evt.which : evt.keyCode;
+          if (charCode != 46 && charCode > 31 
+            && (charCode < 48 || charCode > 57))
+             return false;
+
+          return true;
+       }
+       
 // Special Buttons
 function clearEverythingFunction(){
 	console.log("Function clearEverythingFunction activated.");
